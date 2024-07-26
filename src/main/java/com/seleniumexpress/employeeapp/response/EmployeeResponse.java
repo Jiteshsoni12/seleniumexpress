9c2561095e86377fc8341e77096cb69babbb30cb
Jiteshsoni12 <jiteshsoni12.js@gmail.com>
2024-07-26 09:50:26 +0530

    [Master] With rest template rest call

---
+++ src/main/java/com/seleniumexpress/employeeapp/response/EmployeeResponse.java
@@ -0,0 +1,53 @@
+package com.seleniumexpress.employeeapp.response;
+
+public class EmployeeResponse {
+
+    private Integer id;
+    private String name;
+
+    private String email;
+
+    private String bloodGroup;
+
+    private AddressResponse addressResponse;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public AddressResponse getAddressResponse() {
+        return addressResponse;
+    }
+
+    public void setAddressResponse(AddressResponse addressResponse) {
+        this.addressResponse = addressResponse;
+    }
+}
